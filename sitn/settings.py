f1e16057525b5850fd4864199f3a4ec5f421f5c1
maltaesousa <stephane.maltaesousa@ne.ch>
2026-06-04 17:33:45 +0200

    fix demo redirect and update dotnetid

--- sitn/settings.py
+++ sitn/settings.py
@@ -141,6 +141,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_SRID = 2056
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 OLWIDGET = {
     "globals": {
         "srid": DEFAULT_SRID,
